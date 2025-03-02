5ff8db27da066c86a07ab207f249b06ea894f7d0
reginedizon <rdglipo@paterostechnologicalcollege.edu.ph>
2025-03-02 08:45:00 -0800

    Update Activity1.java

--- Activity1.java
+++ Activity1.java
@@ -1,1 +1,8 @@
-
+public class Activity1 {
+    public static void main(String[] args) {
+        System.out.println("List of Students:");
+   
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Regine Glipo");
+    }
+}
